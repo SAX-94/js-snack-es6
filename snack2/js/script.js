a20669c0083e41a4f8a4bca1daa12e4fdc75dda0
SAX_94 <matteosacchi94@gmail.com>
2022-06-06 21:25:44 +0200

    created array with soccer team as objects

---
+++ snack2/js/script.js
@@ -0,0 +1,30 @@
+
+// Array with soccer teams
+
+const soccerTeams = [
+    {
+        name: "Milan",
+        scores: 0,
+        fails: 0
+    },
+    {
+        name: "Inter",
+        scores: 0,
+        fails: 0
+    },
+    {
+        name: "Juventus",
+        scores: 0,
+        fails: 0
+    },
+    {
+        name: "Roma",
+        scores: 0,
+        fails: 0
+    },
+    {
+        name: "Napoli",
+        scores: 0,
+        fails: 0
+    },
+]
