9f8a628764efd305681a1cf7f2ad14ea8daaedb2
SAX_94 <matteosacchi94@gmail.com>
2022-06-06 16:27:44 +0200

    created array with objects describing bicylces

---
+++ snack1/js/script.js
@@ -0,0 +1,30 @@
+
+/* Array with objects that describe racing bycicles.
+Weights are expressed in kilograms */
+
+const racingBikes = [
+    {
+        name: "Specialized Venge Pro",
+        weight: 7.4
+    },
+    {
+        name: "Cube Attain GTC Race",
+        weight: 8.1
+    },
+    {
+        name: "Colnago CLX",
+        weight: 8.4
+    },
+    {
+        name: "Cannondale Synapse",
+        weight: 8.5
+    },
+    {
+        name: "Vitus Zenium CR",
+        weight: 8.9
+    },
+    {
+        name: "Trek Domane SL5",
+        weight: 9.7
+    },
+]
