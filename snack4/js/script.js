4de3907f846cb5d867665796619e2eb9431785dc
SAX_94 <matteosacchi94@gmail.com>
2022-06-07 21:59:23 +0200

    selected students with grades above 70 and ids over 120

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -10,9 +10,13 @@ let students = [
 
 
 // Capitalize every student name
-const studentCapitalizedNames = students.map(({name}) => name.toUpperCase());
+const studentCapitalizedNames = students.map(({ name }) => name.toUpperCase());
 console.log(studentCapitalizedNames);
 
 // Select students who have grades above 70
-const gradesOver70 = students.filter(({grades}) => grades > 70);
+const gradesOver70 = students.filter(({ grades }) => grades > 70);
 console.log(gradesOver70);
+
+// Select students who have grades above 70 and IDs over 120
+const gradesOver70IdOver120 = students.filter(({ grades, id }) => grades > 70 && id > 120);
+console.log(gradesOver70IdOver120);
